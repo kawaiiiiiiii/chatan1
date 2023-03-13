65a3a61dc0de268affcd5aab0f6311aca8839e81
DESKTOP-62NBI5U\yshkw <yshkwi.chacha@gmail.com>
2023-03-13 20:59:40 +0900

    投稿ゾーン

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/scss/">
+    <title>chatan</title>
+</head>
+
+<body>
+    <main>
+        <h2>Latest Articles</h2>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a><!--リードもあの- -->
+        </div>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a>
+        </div>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a>
+        </div>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a>
+        </div>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a>
+        </div>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a>
+        </div>
+        <div class="articles">
+            <img src="./folder/post_img_1.png" alt="北谷画像">
+            <span>2018/5/20</span>
+            <p>おしゃれカフェがありますよ</p>
+            <a href="">READ MORE</a>
+        </div>
+    </main>
+    <footer>
+        <span>Copyrigth <small>&copy;</small>Shinohara.ALL RIGHTS RESERVED.</span>
+    </footer>
+</body>
+
+</html>
